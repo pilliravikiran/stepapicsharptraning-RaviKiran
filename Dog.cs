4d81e1f15d18ffa13d796e46c6bb29e44c4ea11e
Ravi Kiran <mail2pilliravikiran@gmail.com>
2021-06-13 07:00:38 -0700

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartingC
+{
+    class Dog
+    {
+        private String name;
+       
+        public Dog(String name)
+        {
+            Console.WriteLine("the firstdog name is {0}", name);
+
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        static void Main()
+        {
+            String firstdogname = null;
+            Console.Write("ENtter the name of the first dog");
+            firstdogname = Console.ReadLine();
+            Dog firstdog = new Dog(firstdogname);
+
+
+            
+
+            
+
+
+
+
+        }
+    }
+}
